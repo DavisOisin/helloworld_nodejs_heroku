e0ccc07865f515aa1ac905833c805c9b28edb5d6
DavisOisin <71603514+DavisOisin@users.noreply.github.com>
2020-10-28 19:56:51 +0000

    added some style

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var server = http.createServer(function (req, res)
     {
         //write hello world to web page
         res.writeHead(200, {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/html'
         });
-        res.write('Hello World!!!!!!!!! - Oisin', 'utf-8');
+        res.write('<h1 style="color: red;">Hello World!!!!!!!!! - Oisin</h1>', 'utf-8');
         //close
         res.end();
  
