c335e775c1cac4d704cb9b05e341bcfea5c71841
DavisOisin <71603514+DavisOisin@users.noreply.github.com>
2020-10-28 19:26:09 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// use the http module
+var http = require('http'),
+// find port used by heroku and assign to variable
+port = process.env.PORT || process.argv[2] || 8080;
+//create the server
+var server = http.createServer(function (req, res)
+    {
+        //write hello world to web page
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        //close
+        res.end();
+ 
+    });
+//set server to listen on heroku port found through env
+server.listen(port)
