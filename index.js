9ad6d24cf5542180f313a79f3021aca60e6cd80a
DavisOisin <71603514+DavisOisin@users.noreply.github.com>
2020-10-28 20:01:57 +0000

    test port

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var server = http.createServer(function (req, res)
             'Content-Type': 'text/html'
         });
         res.write('<h1 style="color: red;">Hello World!!!!!!!!! - Oisin</h1>', 'utf-8');
+        res.write(port, 'utf-8');
         //close
         res.end();
  
